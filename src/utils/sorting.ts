fcae3e9079b06470a3946cd73b251c1459a23e90
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-11-23 19:12:25 -0500

    Add: Selection Sort

--- src/utils/sorting.ts
+++ src/utils/sorting.ts
@@ -1,13 +1,33 @@
-import swap from './swap.js';
+import swap from './swap';
 
-// O(n^2)
+// Worst case = O(n^2) Best case O(n)
 export const bubbleSort = (arr: number[]): number[] => {
+  let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        swap(arr, i, j);
+        swap(arr, j, j + 1);
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
+  return arr;
+};
+
+// Worst case = O(n^2) Best case O(n)
+export const selectionSort = (arr: number[]): number[] => {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+
+    swap(arr, i, lowest);
+  }
+
   return arr;
 };
